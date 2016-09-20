87786d8a158458066ab7c930e0e11190372b5b0d
Stephan Valois <stephanvalois@gmail.com>
2016-09-20 21:53:50 +0200

    added searchengine, and process json

--- lib/wiki_search/cli.ex
+++ lib/wiki_search/cli.ex
@@ -4,6 +4,7 @@ defmodule WikiSearch.CLI do
   def main(args) do
     parse_args(args)
     |> process
+    |> extract_from_body
   end
 
   def parse_args(args) do
@@ -27,7 +28,26 @@ defmodule WikiSearch.CLI do
   end
 
   def process({search_term}) do
-    IO.puts search_term
+    WikiSearch.SearchEngine.fetch(search_term)
+  end
+
+  def decode_response({:ok, body}), do: IO.puts is_list(body)
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching search term"
+  end
+
+  def extract_from_body(map) do
+    case map do
+      {_, map_body} ->
+      IO.inspect  Map.keys(map_body)
+      IO.inspect Map.has_key?(map_body, "query")
+      IO.inspect Map.get(map_body, "query")
+    end
+  end
+
+
+  def hale_json({:ok, %{status_code: 200, body: body}}) do
+    {:ok, Poison.Parser.parse!(body)}
   end
 
 end
